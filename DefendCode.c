8a76fe286a4a170e25e5e68b02830a3c0d5c49a4
Combs <primefrozen@hotmail.com>
2018-02-18 20:22:41 -0800

    current working version

--- DefendCode.c
+++ DefendCode.c
@@ -3,17 +3,48 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <regex.h>
+#include <limits.h>
 
 char * _firstName;
 regex_t regex;
 char * _lastName;
+const int INT_OVERFLOW = 1;
 
-void removeNewLine(char * input)
+void error_log(char * error)
+{
+    FILE *errorLog;
+    char *mode = "rw";
+
+    errorLog = fopen("errorlog.txt", mode);
+
+    if(errorLog == NULL)
+    {
+        printf("Can't open errorlog.txt program terminated ");
+        exit(1);
+    }
+    else
+        fprintf(errorLog, "%s", error);
+    
+    fclose(errorLog);
+}
+
+
+void remove_new_line(char * input)
 {
     char *p;
 
     if((p = strchr(input, '\n')) != NULL)
         *p = '\0';
+}
+
+int is_safe_add(int a, int b)
+{
+    if(a > 0 && b > INT_MAX - a)
+        return 1;
+    else if(a < 0 && b < INT_MIN - a)
+        return 1;
+    else 
+        return 0;
 }
 
 void readName()
@@ -23,7 +54,7 @@ void readName()
     printf("Please input your first name. First name must be at most 50 characters long cannot contain numbers or special characters: ");
     fgets(input, 50, stdin);
 
-    removeNewLine(input);
+    remove_new_line(input);
 
     correctInputMatch = regcomp(&regex, "^[a-z|A-Z|\\-]{1,50}$", REG_EXTENDED);
     if(correctInputMatch ){printf("no work "); exit(1);}
@@ -34,7 +65,7 @@ void readName()
     {
         printf("Invalid input try again ");
         fgets(input,50,stdin);
-        removeNewLine(input);
+        remove_new_line(input);
         correctInputMatch = regexec(&regex,input,0,NULL,0);    
     }
 
@@ -45,7 +76,7 @@ void readName()
     printf("Please input your last name. Last name must be at most 50 characters long cannot contain numbers or special characters: ");
     fgets(input, 50, stdin);
 
-    removeNewLine(input);
+    remove_new_line(input);
     correctInputMatch = regexec(&regex,input,0,NULL,0);
 
 
@@ -53,7 +84,7 @@ void readName()
     {
         printf("Invalid input try again ");
         fgets(input,50,stdin);
-        removeNewLine(input);
+        remove_new_line(input);
         correctInputMatch = regexec(&regex,input,0,NULL,0);    
     }
 
@@ -64,7 +95,38 @@ void readName()
     regfree(&regex);
 }
 
+void gather_ints_from_user()
+{
+    int numOne = 0;
+    int numTwo = 0;
+    char input[10];
+    int validInput;
 
+    validInput = regcomp(&regex, "^[0-9]*$", REG_EXTENDED);
+    if(validInput ){printf("no work "); exit(1);}
+
+    do
+    {
+        printf("Input an integer. Value MUST be less than -2,147,483,647 or greater than 2,147,483,647 ");
+        fgets(input, 10, stdin);
+        remove_new_line(input);
+
+        validInput = regexec(&regex,input,0,NULL,0);
+
+        while(validInput == REG_NOMATCH)
+        {
+            printf("Invalid input. Try again ");
+            fgets(input, 10, stdin);
+            remove_new_line(input);
+
+            validInput = regexec(&regex,input,0,NULL,0);
+        }
+
+        numOne = (int)strtol(input, NULL, 10);
+
+    }while(is_safe_add(numOne, numTwo) != INT_OVERFLOW);
+
+}
 
 
 void freeMemory()
@@ -79,8 +141,8 @@ void freeMemory()
 
 int main()
 {
-    readName();
-    
+    //readName();
+    gather_ints_from_user();
     freeMemory();
     return 0;
 }
